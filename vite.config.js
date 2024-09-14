1541c9f922bba950a7c04002701a0bef791b9555
Giuseppe Ernandez <giuseppeernandez@hotmail.it>
2024-09-14 11:56:14 +0200

    removed leftovers from vite config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,5 @@
 // vite.config.js
 import { defineConfig } from "vite";
-import dns from "node:dns";
-
-dns.setDefaultResultOrder("verbatim");
 
 export default defineConfig({
   assetsInclude: ["**/*.wav", "**/*.csd"],
